1a35d453597ceb90407f4434f676abfeb3624939
Sun Liwen <liwen@cube0.tuijianbao.net>
2013-09-16 04:43:42 +0800

    fix default url

--- weiboapp/app/urls.py
+++ weiboapp/app/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from .views import AuthView
 
 urlpatterns = patterns('', 
-    url(r'^auth$', AuthView.as_view(), name='auth'),
+    url(r'^$', AuthView.as_view(), name='auth'),
 )
