d2f967efbe1f5be3c99aeb7798e16b99ba73c43e
Sun Liwen <sunliwen@gmail.com>
2013-09-17 20:46:01 +0800

    fix auth, status/upload, friendships/create

--- weiboapp/app/urls.py
+++ weiboapp/app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from .views import AuthView
+from .views import AuthView, WeiboView
 
 urlpatterns = patterns('', 
     url(r'^$', AuthView.as_view(), name='auth'),
+    url(r'^weibo$', WeiboView.as_view(), name='weibo'),
 )
--- weiboapp/app/views.py
+++ weiboapp/app/views.py
@@ -1,4 +1,4 @@
-import time, json, hmac, base64, logging, hashlib
+import time, json, hmac, base64, logging, hashlib, os
 from datetime import datetime, tzinfo, timedelta
 
 from django.http import HttpResponse
@@ -9,7 +9,30 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 from weibo import APIError, APIClient
-from weiboapp.settings import APP_KEY, APP_SECRET, APP_URL
+from weiboapp.settings import APP_KEY, APP_SECRET, APP_URL, WEIBO_ID
+
+from django.utils import simplejson as json
+
+
+class JSONResponseMixin(object):
+    def render_to_response(self, context):
+        "Returns a JSON response containing 'context' as payload"
+        return self.get_json_response(self.convert_context_to_json(context))
+
+    def get_json_response(self, content, **httpresponse_kwargs):
+        "Construct an `HttpResponse` object."
+        return HttpResponse(content,
+                                 content_type='application/json',
+                                 **httpresponse_kwargs)
+
+    def convert_context_to_json(self, context):
+        "Convert the context dictionary into a JSON object"
+        # Note: This is *EXTREMELY* naive; in reality, you'll need
+        # to do much more complex handling to ensure that arbitrary
+        # objects -- such as Django model instances or querysets
+        # -- can be serialized as JSON.
+        return json.dumps(context)
+
 
 class AuthView(View):
 
@@ -18,7 +41,7 @@ class AuthView(View):
         return super(AuthView, self).dispatch(*args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        return TemplateResponse(request, 
+        return TemplateResponse(request,
             template="auth.html",
             context=dict(
                 APP_KEY=APP_KEY,
@@ -30,29 +53,74 @@ class AuthView(View):
         print request.POST
 
         client = _create_client()
-        data = client.parse_signed_request(request.POST['signed_request'])
+        signed_request = request.POST['signed_request']
+        data = client.parse_signed_request(signed_request)
         print data
 
         user_id = data.get('uid', '')
         auth_token = data.get('oauth_token', '')
+
         if not user_id or not auth_token:
             return TemplateResponse(
-                request, 
+                request,
                 template="auth.html",
                 context=dict(
                     APP_KEY=APP_KEY,
                     APP_URL=APP_URL
                 )
             )
-        
+
         return TemplateResponse(
-                request, 
+                request,
                 template="index.html",
                 context=dict(
                     APP_KEY=APP_KEY,
-                    APP_URL=APP_URL
+                    APP_URL=APP_URL,
+                    SIGNED_REQUEST=signed_request,
                 )
             )
+
+
+class WeiboView(View, JSONResponseMixin):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(WeiboView, self).dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        print request.POST
+
+        client = _create_client()
+        signed_request = request.POST['signed_request']
+        follow = request.POST['signed_request']
+        data = client.parse_signed_request(signed_request)
+
+        user_id = data.get('uid', '')
+        auth_token = data.get('oauth_token', '')
+
+        if not user_id or not auth_token:
+            return JSONResponseMixin.render_to_response(self, dict(error='bad_signature'))
+
+        expires = data.expires
+        client.set_access_token(auth_token, expires)
+
+        # TODO, switch to update_url_text api later
+        pic_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/images/index_small.jpg'))
+        print pic_path
+        f = open(pic_path, 'rb')
+        r = client.statuses.upload.post(status=request.POST['text'], pic=f)
+        f.close()
+
+        if follow:
+            try:
+                r = client.friendships.create.post(uid=WEIBO_ID)
+            except:
+                pass
+
+        print r
+        if 'error' in r:
+            return JSONResponseMixin.render_to_response(self, r)
+        return JSONResponseMixin.render_to_response(self, dict(error='success'))
 
 
 def _create_client(oauth_token=None, expires=None):
